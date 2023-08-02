833807bdfc21b599a4dc136013ad17033e6ae1b9
just4sad <Billiontion@gmail.com>
2023-08-02 14:25:10 +0200

    You need to save the file first before excute the code!!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout << "Hello to you!!!";
+    cout << "Hello to you, kinda trouble to run the code, but at least it workded\n";
+    cout << "Hold on, hold on, it workded?\n";
+    cout << "I need to save the file before i excute the code?";
     return 0;
 }
