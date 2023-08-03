a111427f4d6276f75a0405d75ebd5fe43934d504
just4sad <Billiontion@gmail.com>
2023-08-03 14:29:03 +0200

    Let's start the day

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     cout << "Hello to you, kinda trouble to run the code, but at least it workded\n";
     cout << "Hold on, hold on, it workded?\n";
-    cout << "I need to save the file before i excute the code?";
+    cout << "I need to save the file before i excute the code?\n";
+    cout << "Hello agian";
     return 0;
 }
