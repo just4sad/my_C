92b816c2c1fc4386b7874cf2d26db16bac586804
just4sad <Billiontion@gmail.com>
2023-08-04 18:08:00 +0200

    nothing new

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include<iostream>
 using namespace std;
-int main(){
-    cout << "Hello to you, kinda trouble to run the code, but at least it workded\n";
-    cout << "Hold on, hold on, it workded?\n";
-    cout << "I need to save the file before i excute the code?\n";
-    cout << "Hello agian";
-    return 0;
+
+//值传递
+void swap1(int a ,int b)
+{
+	int temp = a;
+	a = b; 
+    cout << "a= " << a << endl;
+	b = temp;
+    cout << "b= " << b << endl;
+}
+//地址传递
+void swap2(int * p1, int *p2)
+{
+	int temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
+
+int main() {
+
+	int a = 10;
+	int b = 20;
+	swap1(a, b); // 值传递不会改变实参
+
+	//swap2(&a, &b); //地址传递会改变实参
+
+	cout << "a = " << a << endl;
+
+	cout << "b = " << b << endl;
+
+
+	return 0;
 }
